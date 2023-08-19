16ac50e73ef0d84e309ae48bfc8c6c9634e96050
celinaisthegreat <duckhumanperson@gmail.com>
2023-08-19 11:56:07 -0600

    f

---
+++ Flask/basic.py
@@ -0,0 +1,60 @@
+isUppercase = False
+isNumbers = False
+isSymbols = False
+
+isUppercase = bool(input("Do you want uppercase letters? "))
+isNumbers = bool(input("Do you want numbers? "))
+isSymbols = bool(input("Do you want symbols? "))
+
+print(isUppercase)
+print(isNumbers)
+print(isSymbols)
+
+choices = list("abcdefghijklmnopqrstuvwxyz")
+print(choices)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
