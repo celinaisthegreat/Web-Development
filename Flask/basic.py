e4f1719a30d9e518d95cef484810e76360236d0a
celinaisthegreat <duckhumanperson@gmail.com>
2023-08-24 20:05:10 -0600

    a

--- Flask/basic.py
+++ Flask/basic.py
@@ -5,16 +5,25 @@ isSymbols = False
 isUppercase = bool(input("Do you want uppercase letters? "))
 isNumbers = bool(input("Do you want numbers? "))
 isSymbols = bool(input("Do you want symbols? "))
+length = int(input("How many characters do you want?"))
 
-print(isUppercase)
-print(isNumbers)
-print(isSymbols)
+import random
+def generate_password():
+    choices = list("abcdefghijklmnopqrstuvwxyz")
 
-choices = list("abcdefghijklmnopqrstuvwxyz")
-print(choices)
+    if isUppercase == True:
+        choices.extend("ABCDEFGHIJKLMOPQRSTUVWXYZ")
+    if isNumbers == True:
+        choices.extend("0123456789")
+    if isSymbols == True:
+        choices.extend('''"'[]|\~!@#{}$/%^&*()_-+=<>,.:;?''')
 
+    mypassword = ""
+    for i in range(length):
+        mypassword += random.choice(choices)
 
-
+    print(mypassword)
+generate_password()
 
 
 
