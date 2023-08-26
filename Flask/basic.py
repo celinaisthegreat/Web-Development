6aaf947134de405a9401f82032f8d61d6f293fa4
celinaisthegreat <duckhumanperson@gmail.com>
2023-08-26 12:17:23 -0600

    f

--- Flask/basic.py
+++ Flask/basic.py
@@ -1,38 +1,40 @@
-isUppercase = False
-isNumbers = False
-isSymbols = False
+# isUppercase = False
+# isNumbers = False
+# isSymbols = False
 
-isUppercase = bool(input("Do you want uppercase letters? "))
-isNumbers = bool(input("Do you want numbers? "))
-isSymbols = bool(input("Do you want symbols? "))
-length = int(input("How many characters do you want?"))
+# isUppercase = bool(input("Do you want uppercase letters? "))
+# isNumbers = bool(input("Do you want numbers? "))
+# isSymbols = bool(input("Do you want symbols? "))
+# length = int(input("How many characters do you want?"))
+
+# import random
+# def generate_password():
+#     choices = list("abcdefghijklmnopqrstuvwxyz")
+
+#     if isUppercase == True:
+#         choices.extend("ABCDEFGHIJKLMOPQRSTUVWXYZ")
+#     if isNumbers == True:
+#         choices.extend("0123456789")
+#     if isSymbols == True:
+#         choices.extend('''"'[]|\~!@#{}$/%^&*()_-+=<>,.:;?''')
+
+#     mypassword = ""
+#     for i in range(length):
+#         mypassword += random.choice(choices)
+
+#     print(mypassword)
+# generate_password()
 
 import random
-def generate_password():
-    choices = list("abcdefghijklmnopqrstuvwxyz")
+from flask import render_template, Flask
 
-    if isUppercase == True:
-        choices.extend("ABCDEFGHIJKLMOPQRSTUVWXYZ")
-    if isNumbers == True:
-        choices.extend("0123456789")
-    if isSymbols == True:
-        choices.extend('''"'[]|\~!@#{}$/%^&*()_-+=<>,.:;?''')
+app = Flask(_name_)
 
-    mypassword = ""
-    for i in range(length):
-        mypassword += random.choice(choices)
+@app.route('')
+def index():
+    return render_template('home.html')
 
-    print(mypassword)
-generate_password()
-
-
-
-
-
-
-
-
-
+if _name_=="main":
 
 
 
